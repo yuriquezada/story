5a9c77ce5a4d9a1ccad149b6747e25c85346bc7d
yuriquezada <yuri_quezada@hotmail.com>
2019-10-07 00:16:15 -0500

    Creando Login y Sign Up

--- php-login/index.php
+++ php-login/index.php
@@ -1,3 +1,28 @@
-<?php
-echo "Hello mundote"
-?>
+<?php include("../includes/header.php")?>
+
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Store</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="form-inline my-2 my-lg-0 ml-auto">
+            <a href="login.php" class="btn btn-primary mr-2">Log in</a>
+            <a href="signin.php" class="btn btn-primary">Sign up</a>
+          </form>
+        </div>
+      </nav>
+    </header>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm text-center">
+            <h1>Bienvenidos</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php include("../includes/footer.php")?>
